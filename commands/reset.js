2d183a5c77e324023548c3c681999caa7ebf81c5
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-07-22 04:07:23 +0800

    uypdae reset comand

--- commands/reset.js
+++ commands/reset.js
@@ -1,8 +1,18 @@
 export default {
   name: 'reset',
   command: async (db, msg, args) => {
-    db.delete(msg.author.id);
-    msg.reply('get resetted 🤣');
-    db.write();
+    if (msg.author.id === '960911499621179402') {
+      const id =
+        args.length >= 2
+          ? // checks for mention
+            msg.mentions.users.size > 0
+            ? msg.mentions.users.first().id
+            : args[1]
+          : msg.author.id;
+
+      db.delete(id);
+      msg.reply(`get resetted <@${id}> 🤣`);
+      db.write();
+    }
   },
 };
